f572c7e02115fd79a7dbe426d8fe195f2a61d22f
EmmaDeste <emma_deste@hotmail.com>
2020-12-21 20:44:44 +0100

    essai avec un menu

--- harmony.py
+++ harmony.py
@@ -2,9 +2,18 @@ import tkinter as tk
 
 glob_racine = tk.Tk()
 
+def donothing():
+    print ("Rien a faire")
+
 def mise_en_place_IHM():
     global glob_racine
     glob_racine.title("Capucine and Emma")
+    barre_menu=tk.Menu(glob_racine)
+    glob_racine.config(menu=barre_menu)
+    menu_fichier=tk.Menu(barre_menu)
+    menu_fichier.add_command(label="Quitter", command=glob_racine.quit)
+    barre_menu.add_cascade(label="coucou",menu=menu_fichier)
+
 
 mise_en_place_IHM()
 glob_racine.mainloop()

52901f04a2281a8b6fbde498535ed8d05f2c5f9c
CapucineFoucher <capucinefoucher@gmail.com>
2020-12-22 19:09:38 +0100

    Chiffrage des notes et des durées

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
-import simpleaudio as sa
-import simpleaudio.functionchecks as fc
+partition1 = "SOLc p Zc SOLn LAn SOLn DOn Zc SIb"
+partition = partition1.split(' ')
+Note = []
+Duree = []
+
+def transformation(L, N, D: list):
+    dico_n = {"DO": 0, "RE": 2, "MI": 4, "FA": 5, "SOL": 7, "LA": 9, "SI": 11, "Z": -1, "": -2}
+    dico_d = {"c": 2, "n": 4, "b": 8, "r": 16}
+    for x in L:
+        note = ""
+        duree = ""
+        for y in x:
+            if 'A' <= y <= 'Z':
+                note = note + y
+            else:
+                duree = duree + y
+        N.append(note)
+        D.append(duree)
+    nc = []
+    for s in N:
+        if dico_n[s] > -2:
+            nc.append(dico_n[s])
+    print("nc=", nc)
+    dc = []
+    for s in D:
+        if s == "p":
+            dc[-1] += dc[-1]//2
+        else:
+            dc.append(dico_d[s])
+    print("dc=", dc)
+    return (nc, dc)
 
 
-# This is a sample Python script.
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    fc.LeftRightCheck.run()
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(transformation(partition, Note, Duree))

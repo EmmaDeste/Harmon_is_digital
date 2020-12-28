38f78f450112b76a0595e77bd45d64732673b79b
EmmaDeste <emma_deste@hotmail.com>
2020-12-28 13:24:44 +0100

    capacité de changer la vitesse du morceau de musique + en-tête de fichier

--- principal.py
+++ principal.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+"""
+Project: Harmony is digital
+Authors: Capucine Foucher and Emma Desté
+Promo: EFREI 2025
+Role: this is the main file in charge of :
+- setting the graphic interface,
+- read songs collection from a file,
+- write songs collection in a file,
+- create digital sound using numpy,
+- play sound using simpleaudio.
+"""
+
 from util_notes import *
 
 import tkinter as tk
@@ -26,6 +38,10 @@ glob_canvas1 = 0
 glob_canvas2 = 0
 glob_po = 0
 glob_lbsong = 0
+glob_croche_duration = 0.125  # a croche has a musical duration of 125ms
+glob_croche_dur_sv = tk.StringVar()
+
+sample_rate = 8000
 
 def sound_test_loudspeakers():
     """sound test on left and right loudspeakers"""
@@ -111,10 +127,6 @@ def markov_chain_v2():
     markov_song_v2 = (title, noteduree_to_partition(nc, dc))
     addition_if_needed(markov_song_v2)
 
-def vitesse():
-    # not sure we keep that
-    pass
-
 
 def play_song():
     """
@@ -135,13 +147,23 @@ def play_song():
         n = inversion(n)
 
     title_current_song = glob_collection[song_num][0]
-    addition_if_needed((creation_de_title(title_current_song, inv, tr),noteduree_to_partition(n,d)))
+    addition_if_needed((title_creation(title_current_song, inv, tr), noteduree_to_partition(n,d)))
+
+    set_croche_duration(glob_croche_dur_sv.get())
+    print("glob_croche_duration : ", glob_croche_duration, "glob_croche_dur_sv : ", glob_croche_dur_sv.get())
 
     f = frequences(n)
     play_notes(f, d)
 
 
 def title_creation(title_current_song, inv, tr):
+    """
+    role:
+    :param title_current_song:
+    :param inv:
+    :param tr:
+    :return res:
+    """
     res = title_current_song
     if inv == 1:
         res += " reverse"
@@ -151,45 +173,63 @@ def title_creation(title_current_song, inv, tr):
 
 
 def controls_settings():
+    """
+    role:
+    no param:
+    no return:
+    effect on the program:
+    """
     global glob_root, glob_transpo
     # transposition
     le = tk.Label(text="transposition")
-    le.grid(row=0, column=2, sticky=tk.W)
+    le.grid(row=1, column=2, sticky=tk.W)
     e = tk.Entry(width=3, textvariable=glob_transpo)
-    e.grid(row=0, column=1, sticky=tk.E)
+    e.grid(row=1, column=1, sticky=tk.E)
     glob_transpo.set('0')
+
     # inversion
-    cb=tk.Checkbutton(text="inversion", variable=glob_inv)
-    cb.grid(row=1, column=1, columnspan=2)
+    cb = tk.Checkbutton(text="inversion", variable=glob_inv)
+    cb.grid(row=2, column=1, columnspan=2)
     glob_inv.set('0')
 
-    #jouer
-    bj=tk.Button(text="Play", command=play_song())
-    bj.grid(row=0, column=3)
+    # croche duration
+    cd = tk.Label(text="croche duration in ms")
+    cd.grid(row=0, column=2, sticky=tk.W)
+    d = tk.Entry(width=5, textvariable=glob_croche_dur_sv)
+    d.grid(row=0, column=1, sticky=tk.E)
+    glob_croche_dur_sv.set('125')
+
+    # play
+    bplay = tk.Button(text="Play", command=play_song)
+    bplay.grid(row=1, column=3)
 
 
 def menus_settings():
+    """
+    role:
+    no param:
+    no return:
+    effect on the program:
+    """
     global glob_root
     glob_root.title("Capucine and Emma")
     menu_bar = tk.Menu(glob_root)
     glob_root.config(menu=menu_bar)
 
     menu_markov = tk.Menu(menu_bar)
-    menu_markov.add_command(label="Version 1", command=chaine_de_markov_v1)
-    menu_markov.add_command(label="Version 2", command=chaine_de_markov_v2)
+    menu_markov.add_command(label="Version 1", command=markov_chain_v1)
+    menu_markov.add_command(label="Version 2", command=markov_chain_v2)
     menu_bar.add_cascade(label="Markov chains", menu=menu_markov)
 
     menu_avance = tk.Menu(menu_bar)
-    menu_avance.add_command(label="Sound test loudspeakers", command=sound_test_loudspeakers())
-    menu_avance.add_command(label="Sound test superposition", command=sound_test_superposition())
+    menu_avance.add_command(label="Sound test loudspeakers", command=sound_test_loudspeakers)
+    menu_avance.add_command(label="Sound test superposition", command=sound_test_superposition)
     menu_avance.add_separator()
     menu_avance.add_radiobutton(label="Instrument: sinus", var=glob_instrum, value="Sinus")
     menu_avance.add_radiobutton(label="Instrument: rectangles", var=glob_instrum, value="Rectangles")
     menu_avance.add_radiobutton(label="Instrument: organ", var=glob_instrum, value="Organ")
     menu_avance.add_radiobutton(label="Instrument: UFO", var=glob_instrum, value="UFO")
-    glob_instrum.set("Sinus")  #set the default value
-    menu_avance.add_separator()
-    menu_avance.add_command(label="Speed", command=vitesse)
+    glob_instrum.set("Sinus")  # set the default value
     menu_bar.add_cascade(label="Advanced", menu=menu_avance)
 
 
@@ -197,41 +237,47 @@ def canvas_settings():
     global glob_canvas1, glob_canvas2
     glob_canvas1 = Canvas(width=600, height=50)
     glob_canvas2 = Canvas(width=600, height=300)
-    glob_canvas1.grid(row=2, column=0, columnspan=4)
-    glob_canvas2.grid(row=3, column=0, columnspan=4)
+    glob_canvas1.grid(row=3, column=0, columnspan=4)
+    glob_canvas2.grid(row=4, column=0, columnspan=4)
 
 
-def play_note(frequence, duree_musicale):
+def play_note(frequence, musical_duration):
+    """
+    role:
+    :param frequence:
+    :param musical_duration:
+    no return:
+    effect on the program:
+    """
     global glob_po
     print(frequence)
-    duree = (duree_musicale * 0.125) / 2        #une croche a une durée musicale de 2 et une durée réelle de 125ms
-    sample_rate=8000
-    temps = np.linspace(0, duree, int(duree*sample_rate),False)  # temps est une liste de temps
+    duration = (musical_duration * glob_croche_duration) / 2        # croche has a musical duration of 2
+    time = np.linspace(0, duration, int(duration * sample_rate), False)  # time is a list of time
     if frequence > 0:
         if glob_instrum.get() == "Sinus":
-            signal = np.sin(frequence * temps * 2 * pi)     # 2*pi ==> (6) * np.pi
+            signal = np.sin(frequence * time * 2 * pi)     # Emma: 2*pi ==> (6) * np.pi
         elif glob_instrum.get() == "Rectangles":
-            signal = np.sign(np.sin(temps * 2 * pi * frequence))
+            signal = np.sign(np.sin(time * 2 * pi * frequence))
         elif glob_instrum.get() == "Organ":
-            signal = 0.4 * (np.sin(temps * 2 * pi * frequence)) + 0.5 * (
-                np.sin(temps * 2 * pi * (frequence / 4))) + 0.1 * (np.sin(temps * 2 * pi * (frequence * 4)))
+            signal = 0.4 * (np.sin(time * 2 * pi * frequence)) + 0.5 * (
+                np.sin(time * 2 * pi * (frequence / 4))) + 0.1 * (np.sin(time * 2 * pi * (frequence * 4)))
         elif glob_instrum.get() == "UFO":
-            signal = 0.2 * (np.sin(temps * 2 * pi * frequence)) + 0.4 * (
-                np.sin(temps * 2 * pi * (frequence / sqrt(2)))) + 0.4 * (np.sin(temps * 2 * pi * (frequence * sqrt(2))))
+            signal = 0.2 * (np.sin(time * 2 * pi * frequence)) + 0.4 * (
+                np.sin(time * 2 * pi * (frequence / sqrt(2)))) + 0.4 * (np.sin(time * 2 * pi * (frequence * sqrt(2))))
         else:
             print("Erreur : instrument inconnu")
             return
     else:
-        signal = temps * 0.0
+        signal = time * 0.0
 
-    # enveloppe : important pour différencier à l'oreille 2 même notes qui se suivent
+    # envelope : important to differentiate two following same notes (with ears)
     # TODO : https: // fr.wikipedia.org / wiki / Enveloppe_sonore
-    enveloppe = np.exp(-temps * 3)
-    signal = signal * enveloppe
-    dessine(signal)     #lorsqu'on est encore entre -1 et 1
+    envelope = np.exp(-time * 3)
+    signal = signal * envelope
+    draw(signal)     # when we are between -1 and 1
 
     maxi = np.max(np.abs(signal))
-    if maxi <= 0:    #cas d'un signal nul = silence
+    if maxi <= 0:    # case of null signal : silence
         maxi=1
     signal *= 8388607 / maxi
     signal = signal.astype(np.int32)  # signal sur 16 bits
@@ -246,8 +292,8 @@ def play_note(frequence, duree_musicale):
     audio = bytearray(byte_array)
 
     glob_po = sa.play_buffer(audio, 1, 3, sample_rate)  # playobject
-    #Une fonction play_sheet qui à partir d’une séquence de fréquences et de durées, appelle les fonctions sound et sleep pour lire la partition musicale ??
-    if glob_po != 0:  # attente de la fin d'une éventuelle note précédente
+    #Emma: Une fonction play_sheet qui à partir d’une séquence de fréquences et de durées, appelle les fonctions sound et sleep pour lire la partition musicale ??
+    if glob_po != 0:  # wait for the end of a potential previous note
         glob_po.wait_done()
 
     '''signal,
@@ -255,51 +301,76 @@ def play_note(frequence, duree_musicale):
     nombre d'octets :2 octets = 16 bits,
     fréquence d'échantillonage")'''
 
+def set_croche_duration(duration):
+    # duration in string
+    # pas de return parce que seulement effet de bord sur glob_croche_duration
+    global glob_croche_duration
+    # https://stackoverflow.com/questions/1265665/how-can-i-check-if-a-string-represents-an-int-without-using-try-except
+    print("udration : ", duration, "isdigit: ", duration.isdigit())
+    if duration.isdigit():
+        dur = float(duration)
+        if 0 < dur < 4000:
+            glob_croche_duration = dur / 1000
 
-def dessin2(signal):  # on lui passe le signal entre -1 et 1
+def drawing2(signal):
+    """
+    role:
+    :param signal: signal between -1 and 1
+    no return:
+    """
     glob_canvas2.delete("all")
-    # axe horizontal
-    glob_canvas2.create_line(0,152, 600,152, width=1, fill="red")
-    glob_canvas2.create_line(600,152, 590,142, width=1, fill="red")
-    glob_canvas2.create_line(600,152, 590,162, width=1, fill="red")
+    # horizontal axis
+    glob_canvas2.create_line(0, 152, 600, 152, width=1, fill="red")
+    glob_canvas2.create_line(600, 152, 590, 142, width=1, fill="red")
+    glob_canvas2.create_line(600, 152, 590, 162, width=1, fill="red")
     for i in range(1, 100):
         t = i / 100
-        horizontal = int(t * 8000)
+        horizontal = int(t * sample_rate)
         glob_canvas2.create_line(horizontal+9, 148, horizontal+9, 156, width=1, fill="red")
-        legende = "{0}s".format(t)
-        glob_canvas2.create_text(horizontal+9, 161, text=legende, fill="black")
+        legend = "{0}s".format(t)
+        glob_canvas2.create_text(horizontal+9, 161, text=legend, fill="black")
 
-    # axe vertical
-    glob_canvas2.create_line(9,5, 9,300, width=1, fill="red")
-    glob_canvas2.create_line(9,5, 14,10, width=1, fill="red")
-    glob_canvas2.create_line(9,5, 4,10, width=1, fill="red")
+    # vertical axis
+    glob_canvas2.create_line(9, 5, 9, 300, width=1, fill="red")
+    glob_canvas2.create_line(9, 5, 14, 10, width=1, fill="red")
+    glob_canvas2.create_line(9, 5, 4, 10, width=1, fill="red")
     for i in range(-4, 4):
         y = i / 4
         vertical = 149 * (1 - y) + 3
-        glob_canvas2.create_line(4,vertical, 14,vertical, width=1, fill="red")
-        legende = "{0}".format(y)
-        glob_canvas2.create_text(24,vertical, text=legende, fill="black")
+        glob_canvas2.create_line(4, vertical, 14, vertical, width=1, fill="red")
+        legend = "{0}".format(y)
+        glob_canvas2.create_text(24, vertical, text=legend, fill="black")
 
     y = signal[0]
-    vertic_pre = 149 * (1 - y) + 3
-    for i in range(1, 600):
+    vertical_pre = 149 * (1 - y) + 3
+    last_i = min(len(signal),600)
+    for i in range(1, last_i):
         y = signal[i]
-        vertic = 149 * (1 - y) + 3
-        glob_canvas2.create_line(i-1+9,vertic_pre, i+9, vertic, width=1, fill="blue")
-        vertic_pre = vertic
-        #surement utile sur PC
-    # glob_canvas2.update_idletasks()
-    glob_canvas2.update()
+        vertical = 149 * (1 - y) + 3
+        glob_canvas2.create_line(i-1+9, vertical_pre, i+9, vertical, width=1, fill="blue")
+        vertical_pre = vertical
+    glob_canvas2.update()  # useful on PC ??
 
 
-def play_notes(frequence, duree):
+def play_notes(frequence, duration):
+    """
+    role:
+    :param frequence:
+    :param duration:
+    no return:
+    """
     for i in range(len(frequence)):
-        play_note(frequence[i], duree[i])
+        play_note(frequence[i], duration[i])
 
 
-def dessin1(signal):
+def drawing1(signal):
+    """
+    role:
+    :param signal:
+    no return:
+    """
     glob_canvas1.delete("all")
-    #axe horizontal
+    # horizontal axis
     glob_canvas1.create_line(0, 27, 600, 27, width=1, fill="red")
     glob_canvas1.create_line(600, 27, 590, 22, width=1, fill="red")
     glob_canvas1.create_line(600, 27, 590, 32, width=1, fill="red")
@@ -307,10 +378,10 @@ def dessin1(signal):
         t = i / 10
         horizontal = int(t * 1000)
         glob_canvas1.create_line(horizontal, 23, horizontal, 31, width=1, fill="red")
-        legende = "{0}s".format(t)
-        glob_canvas1.create_text(horizontal, 36, text=legende, fill="black")
+        legend = "{0}s".format(t)
+        glob_canvas1.create_text(horizontal, 36, text=legend, fill="black")
 
-    # AXE VERTICAL : trait, flèche, graduation tout les 0.25 sachant que les valeurs vont de -1 à 1 : donc 0=1 et 50=-1
+    # vertical axis
     glob_canvas1.create_line(0, 27, 600, 27, width=1, fill="red")
     glob_canvas1.create_line(600, 27, 590, 22, width=1, fill="red")
     glob_canvas1.create_line(600, 27, 590, 32, width=1, fill="red")
@@ -318,8 +389,8 @@ def dessin1(signal):
         t = i / 10
         horizontal = int(t * 1000)
         glob_canvas1.create_line(horizontal, 23, horizontal, 31, width=1, fill="red")
-        legende = "{0}s".format(t)
-        glob_canvas1.create_text(horizontal, 36, text=legende, fill="black")
+        legend = "{0}s".format(t)
+        glob_canvas1.create_text(horizontal, 36, text=legend, fill="black")
 
     y = signal[0]
     vertical_pre = 24 * (1 - y) + 3
@@ -327,31 +398,39 @@ def dessin1(signal):
         t = h / 1000
         ind = int(t * 8000)
         if ind >= len(signal):
-            break  # arrêter le dessin si on est arriver au bout du signal avant 0.6s
+            break  # stop drawing if the signal finish before 0.6s
         y = signal[ind]
         vertical = 24 * (1 - y) + 3
         glob_canvas1.create_line(h - 1, vertical_pre, h, vertical, width=1, fill="blue")
         vertical_pre = vertical
-         #sûrement utile sur PC
-    # glob_canvas1.update_idletasks()
-    glob_canvas1.update()
+    glob_canvas1.update()   # useful for PC ??
 
 
-def dessine(signal):
-    dessin1(signal)
-    dessin2(signal)
+def draw(signal):
+    """
+    role:
+    :param signal:
+    no return:
+    """
+    drawing1(signal)
+    drawing2(signal)
 
-def lecture_fichier():  #Une fonction read_line_file(f, num) qui prend en paramètres un nom de fichier et un numéro de ligne et qui retourne le contenu de la ligne en question.
+def file_reading(file_name):  #Emma: Une fonction read_line_file(f, num) qui prend en paramètres un nom de fichier et un numéro de ligne et qui retourne le contenu de la ligne en question.
+    """
+    role:
+    :param file_name:
+    :return collection:
+    """
     collection = []
-    file = open("partitions.txt","r", encoding="utf-8")       #indicate "r"ead only
+    file = open(file_name, "r", encoding="utf-8")  # indicate "r"ead only
     line1 = file.readline()
     # https://qastack.fr/programming/5843518/remove-all-special-characters-punctuation-and-spaces-from-string
-    line1 = re.sub('[^A-Za-z0-9 èéêâ?!#\'\-]+', '', line1)   #+ veut dire qu'on remplace les caractères spéciaux présents au moins une fois
+    line1 = re.sub('[^A-Za-z0-9 èéêâ?!#\'\-]+', '', line1)   # + means it replace special characters present at least once
     line2 = file.readline()
     line2 = re.sub('[^A-Za-z0-9 ]+', '', line2)
-    while line1 != "":             #pour éviter les fins de lignes et fin de fichier, spécial sur Mac
-        collection.append((line1, line2))  # le dernier caractère de line est un passage à la ligne
-        line1 = file.readline()     # /!/ fin de ligne n'est pas codé pareil sur Mac et Windows
+    while line1 != "":                     # to avoid end of lines and end of files, specific for Mac
+        collection.append((line1, line2))  # the last character of the line is a carriage return. /!\ carriage return has a different coding on Windows and on Mac
+        line1 = file.readline()
         line1 = re.sub('[^A-Za-z0-9 èéêâ?!#\'\-]+', '', line1)
         line2 = file.readline()
         line2 = re.sub('[^A-Za-z0-9 ]+', '', line2)
@@ -359,20 +438,26 @@ def lecture_fichier():  #Une fonction read_line_file(f, num) qui prend en param�
     return collection
 
 def setting_songs_list():
+    """
+    role:
+    no param:
+    no return:
+    effect on the program:
+    """
     global glob_lbsong, glob_collection
     lb = tk.Listbox(glob_root, width=40, height=6,
                     selectmode=tk.SINGLE)
     for element in glob_collection:
-        (title,part)=element
-        lb.insert(tk.END,title)     #tk.END est une constante définie qu'on ajoute à la fin d'une liste
-    lb.select_set(0)                #défini la valeur par défaut
-    lb.grid(row=0, column=0)
+        (title, part) = element
+        lb.insert(tk.END, title)  # tk.END is a defined constant that is added at the end of a list
+    lb.select_set(0)              # set the default value
+    lb.grid(row=0, column=0, rowspan=3)
     glob_lbsong = lb
 
 
 controls_settings()
 menus_settings()
-glob_collection=lecture_fichier()
+glob_collection = file_reading("partitions.txt")
 setting_songs_list()
 canvas_settings()
 glob_root.mainloop()
